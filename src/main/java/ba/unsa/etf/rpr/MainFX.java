d573c198dab2c4ffc062e874a34c188e4b8dbbd3
nobucina <nobucina1@etf.unsa.ba>
2022-12-12 13:54:52 +0100

    MainFX added

---
+++ src/main/java/ba/unsa/etf/rpr/MainFX.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class MainFX extends Application {
+
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fl = new FXMLLoader(getClass().getResource(""));
+    }
+}
