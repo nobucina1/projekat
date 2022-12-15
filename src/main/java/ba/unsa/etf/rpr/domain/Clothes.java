3ace615214e866f0f259e458ba4b10d734bc1cbe
nobucina <nobucina1@etf.unsa.ba>
2022-12-15 14:47:01 +0100

    Added atribute price to CLothes

--- src/main/java/ba/unsa/etf/rpr/domain/Clothes.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Clothes.java
@@ -13,6 +13,7 @@ public class Clothes {
     private String clothes_name;
     private int idcategory;
     private int size;
+    private int price;
 
     public int getIdclothes() {
         return idclothes;
@@ -46,17 +47,25 @@ public class Clothes {
         this.size = size;
     }
 
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Clothes clothes = (Clothes) o;
-        return idclothes == clothes.idclothes && idcategory == clothes.idcategory && size == clothes.size && clothes_name.equals(clothes.clothes_name);
+        return idclothes == clothes.idclothes && idcategory == clothes.idcategory && size == clothes.size && price == clothes.price && clothes_name.equals(clothes.clothes_name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(idclothes, clothes_name, idcategory, size);
+        return Objects.hash(idclothes, clothes_name, idcategory, size, price);
     }
 
     @Override
@@ -66,6 +75,7 @@ public class Clothes {
                 ", clothes_name='" + clothes_name + '\'' +
                 ", idcategory=" + idcategory +
                 ", size=" + size +
+                ", price=" + price +
                 '}';
     }
 }
