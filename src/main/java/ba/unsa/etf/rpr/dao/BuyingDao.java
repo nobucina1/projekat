2df91f2b85d732d8d3566f5cdea75fe647628cec
nobucina <nobucina1@etf.unsa.ba>
2022-12-23 11:50:41 +0100

    Interface BuyingDao created

---
+++ src/main/java/ba/unsa/etf/rpr/dao/BuyingDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Buying;
+
+/**
+        * Dao interface for Buying domain bean
+        *
+        * @author Nermin Obucina
+        */
+public interface BuyingDao extends Dao<Buying>{
+}
--- src/main/java/ba/unsa/etf/rpr/dao/ClothesDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ClothesDaoSQLImpl.java
@@ -5,6 +5,7 @@ import ba.unsa.etf.rpr.domain.Clothes;
 import ba.unsa.etf.rpr.exceptions.ShopException;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -15,7 +16,16 @@ public class ClothesDaoSQLImpl extends AbstractDao<Clothes> implements ClothesDa
 
     @Override
     public Clothes row2object(ResultSet rs) throws ShopException {
-        return null;
+        try {
+            Clothes c = new Clothes();
+            c.setId(rs.getInt("idclothes"));
+            c.setClothes_name(rs.getString("clothes_name"));
+
+            return c;
+        } catch (Exception e) {
+            throw new ShopException(e.getMessage(),e);
+        }
+
     }
 
     @Override
@@ -23,16 +33,34 @@ public class ClothesDaoSQLImpl extends AbstractDao<Clothes> implements ClothesDa
         return null;
     }
 
+    /**
+     * Returns all clothes that contains given text.
+     *
+     * @param text search string for clothes
+     * @return list of clothes
+     */
     @Override
     public List<Clothes> searchByText(String text) throws ShopException {
         return null;
     }
 
+    /**
+     * Returns all clothes that are given Category.
+     *
+     * @param category search string for clothes
+     * @return list of clothes
+     */
     @Override
     public List<Clothes> searchByCategory(Category category) throws ShopException {
         return null;
     }
 
+    /**
+     * Returns all clothes that are given size.
+     *
+     * @param size search string for clothes
+     * @return list of clothes
+     */
     @Override
     public List<Clothes> searchBySize(int size) throws ShopException {
         return null;
