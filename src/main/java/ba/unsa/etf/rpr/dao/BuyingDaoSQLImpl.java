7c80013531857baf4889d68882672265b4cb9268
nobucina <nobucina1@etf.unsa.ba>
2022-12-25 16:30:17 +0100

    row2object in BuyingDaoSQLImpl implemented

--- src/main/java/ba/unsa/etf/rpr/dao/BuyingDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BuyingDaoSQLImpl.java
@@ -1,9 +1,11 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Buying;
+import ba.unsa.etf.rpr.domain.Category;
 import ba.unsa.etf.rpr.exceptions.ShopException;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 /**
@@ -17,7 +19,16 @@ public class BuyingDaoSQLImpl extends AbstractDao<Buying> implements BuyingDao{
 
     @Override
     public Buying row2object(ResultSet rs) throws ShopException {
-        return null;
+        try {
+            Buying cat = new Buying();
+            cat.setId(rs.getInt("idbuying"));
+            cat.setDate_of_buying(rs.getDate("date_of_buying"));
+            cat.setIduser(DaoFactory.userDao().getById(rs.getInt("iduser")));
+            cat.setIdclothes(DaoFactory.clothesDao().getById(rs.getInt("idclothes")));
+            return cat;
+        } catch (SQLException e) {
+            throw new ShopException(e.getMessage(), e);
+        }
     }
 
     @Override
