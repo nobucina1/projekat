bca37ccd6f6bdc5a6aabae8f4e5edce73aef0422
nobucina <nobucina1@etf.unsa.ba>
2022-12-29 17:36:00 +0100

    Database added

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,16 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.dao.UserDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.ShopException;
+
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
 }
