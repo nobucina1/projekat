f56bfc693f0d1241006aa32d12c61ad462539166
nobucina <nobucina1@etf.unsa.ba>
2022-12-30 15:31:09 +0100

    getAll method in ClothesManager implemented

--- src/main/java/ba/unsa/etf/rpr/business/ClothesManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ClothesManager.java
@@ -1,5 +1,11 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Clothes;
+import ba.unsa.etf.rpr.exceptions.ShopException;
+
+import java.util.List;
+
 /**
  * Business Logic Layer for Clothes
  *
@@ -7,5 +13,7 @@ package ba.unsa.etf.rpr.business;
  */
 public class ClothesManager {
 
-
+    public List<Clothes> getAll() throws ShopException {
+        return DaoFactory.clothesDao().getAll();
+    }
 }
