0cbdddb6791226cfb9cb0a73a45282acf01fb03c
nobucina <nobucina1@etf.unsa.ba>
2022-12-30 18:27:40 +0100

    validateCategoryName implemented in CategoryManager

--- src/main/java/ba/unsa/etf/rpr/business/CategoryManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CategoryManager.java
@@ -1,9 +1,18 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.exceptions.ShopException;
+
 /**
  * Business Logic Layer for management of Categories
  *
  * @author Nermin Obucina
  */
 public class CategoryManager {
+
+    public void validateCategoryName(String name) throws ShopException {
+        if (name == null || name.length() > 45 || name.length() < 3) {
+            throw new ShopException("Category name must be between 3 and 45 chars");
+        }
+    }
+
 }
