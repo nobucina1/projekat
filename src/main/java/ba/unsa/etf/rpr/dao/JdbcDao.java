01b8b1bb5aa5e81e7e93429b5ef14389bb9b0126
nobucina <nobucina1@etf.unsa.ba>
2023-01-06 14:08:25 +0100

    jdbcdao implemented

--- src/main/java/ba/unsa/etf/rpr/dao/JdbcDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/JdbcDao.java
@@ -1,4 +1,78 @@
 package ba.unsa.etf.rpr.dao;
 
+import java.sql.*;
+
 public class JdbcDao {
+    // Replace below database url, username and password with your actual database credentials
+    private static final String DATABASE_URL = "jdbc:mysql://sql.freedb.tech";
+    private static final String DATABASE_USERNAME = "freedb_nobucina1";
+    private static final String DATABASE_PASSWORD = "#y3q4bffwgJ%HAu";
+    private static final String SELECT_QUERY = "SELECT * FROM user WHERE mail = ? and password = ?";
+    private static final String INSERT_QUERY = "INSERT INTO user (name, surname, mail, password) VALUES (?, ?, ?, ?)";
+
+    public boolean validate(String mail, String password) throws SQLException {
+
+        // Step 1: Establishing a Connection and
+        // try-with-resource statement will auto close the connection.
+        try (Connection connection = DriverManager
+                .getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY)) {
+            preparedStatement.setString(1, mail);
+            preparedStatement.setString(2, password);
+
+            System.out.println(preparedStatement);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return true;
+            }
+
+
+        } catch (SQLException e) {
+            // print SQL exception information
+            printSQLException(e);
+        }
+        return false;
+    }
+
+    public void insertRecord(String name, String surname, String mail, String password) throws SQLException {
+
+        // Step 1: Establishing a Connection and
+        // try-with-resource statement will auto close the connection.
+        try (Connection connection = DriverManager
+                .getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, surname);
+            preparedStatement.setString(3, mail);
+            preparedStatement.setString(4, password);
+
+            System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            // print SQL exception information
+            printSQLException(e);
+        }
+    }
+
+    public static void printSQLException(SQLException ex) {
+        for (Throwable e: ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
 }
